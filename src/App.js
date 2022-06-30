ed0e8e602c44d0cd063aa6f428669d19bc078700
Jonghan Park <jonghan.park@edu.sait.ca>
2022-06-30 12:47:36 -0600

    Add addTodo function

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   }
 
   const completeTodo = async (id) => {
-    const data = fetch(API_BASE + "/todo/complete/" + id)
+    const data = await fetch(API_BASE + "/todo/complete/" + id)
     .then(res => res.json())
 
     setTodos(todos => todos.map(todo => {
@@ -31,6 +31,28 @@ function App() {
       }
       return todo;
     }))
+  }
+
+  const deleteTodo = async id => {
+    const data = await fetch(API_BASE + "/todo/delete/" + id, {
+      method: "DELETE"
+    }).then(res => res.json());
+
+    setTodos(todos => todos.filter(todo => todo._id !== data._id));
+  }
+  
+  const addTodo = async () => {
+    const data = await fetch(API_BASE + "/todo/new", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        text: newTodo
+      })
+    }).then(res => res.json());
+
+    console.log(data);
   }
 
 	return (
@@ -43,10 +65,26 @@ function App() {
         <div className={"todo " + (todo.complete ? "is-complete" : "")} key={todo._id} onClick={() => completeTodo(todo._id)}>
           <div className="checkbox"></div>
           <div className="text">{todo.text}</div>
-          <div className="delete-todo">x</div>
+          <div className="delete-todo" onClick={() => deleteTodo(todo._id)}>x</div>
         </div>
         ))}
       </div>
+      <div className="addPopup" onClick={() => setPopupActive(true)}>+</div>
+      {popupActive ? (
+        <div className="popup">
+          <div className="closePopup" onClick={() => setPopupActive(false)}>x</div>
+          <div className="content">
+            <h3>Add Task</h3>
+            <input 
+            type="text" 
+            className='add-todo-input' 
+            onChange={e => setNewTodo(e.target.value)} 
+            value={newTodo}
+            />
+            <div className="button" onClick={addTodo}>Create Task</div>
+          </div>
+        </div>
+      ) : ''}
 		</div>
 	);
 }
